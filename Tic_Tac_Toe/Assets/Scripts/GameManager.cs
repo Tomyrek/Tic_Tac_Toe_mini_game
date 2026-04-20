fce12174337ed05be59dbcec6d857634f3e5eaf2
Tomyrek <pajko01@gmail.com>
2026-04-20 21:34:40 +0200

    Postavka table materijala skripte grida, game managera, game visual managera i player ui zapocet

---
+++ Tic_Tac_Toe/Assets/Scripts/GameManager.cs
@@ -0,0 +1,85 @@
+using System;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GameManager : NetworkBehaviour
+{
+    public static GameManager Instance { get; private set;}
+
+    public event EventHandler <OnClickedOnGridPositionEventArgs> OnClickedOnGridPosition;
+    public class OnClickedOnGridPositionEventArgs : EventArgs
+    {
+        public int x;
+        public int y;
+        public PlayerType playerType;
+    }
+
+    public enum PlayerType
+    {
+        None,
+        Cross,
+        Circle,
+    }
+
+    private PlayerType localPlayerType;
+    private PlayerType currentPlayablePlayerType;
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Debug.Log("More than one GameManger instance!");
+        }
+        Instance = this;
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        Debug.Log("OnNetworkSpawn: " + NetworkManager.Singleton.LocalClientId);
+        if(NetworkManager.Singleton.LocalClientId == 0 )
+        {
+            localPlayerType = PlayerType.Cross;
+        }
+        else
+        {
+            localPlayerType = PlayerType.Circle;
+        }
+
+        if (IsServer)
+        {
+            currentPlayablePlayerType = PlayerType.Cross;   
+        }
+    }
+
+    [Rpc(SendTo.Server)]
+    public void ClickedOnGridPositionRpc(int x ,int y, PlayerType playerType){
+        Debug.Log("ClickedOnGridPosition "+ x + ", " + y);
+        if(playerType != currentPlayablePlayerType)
+        {
+            return;
+        }
+        
+        
+        OnClickedOnGridPosition?.Invoke(this, new OnClickedOnGridPositionEventArgs{
+            x = x,
+            y = y,
+            playerType = playerType,
+        });
+
+        switch (currentPlayablePlayerType)
+        {
+            default:
+            case PlayerType.Cross:
+                currentPlayablePlayerType = PlayerType.Circle;
+                break;
+            case PlayerType.Circle:
+                currentPlayablePlayerType = PlayerType.Cross;
+                break;
+        }
+    }
+    public PlayerType GetLocalPlayerType()
+    {
+        return localPlayerType;
+    }
+}
---
+++ Tic_Tac_Toe/Assets/Scripts/GameVisualManager.cs
@@ -0,0 +1,48 @@
+using Unity.Netcode;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class GameVisualManager : NetworkBehaviour
+{
+    private const float GRID_SIZE = 3.0f;
+
+    [SerializeField] private Transform crossPrefab;
+    [SerializeField] private Transform circlePrefab;
+
+    private void Start()
+    {
+        GameManager.Instance.OnClickedOnGridPosition += GameManager_OnClickedOnGridPosition;
+    }
+    private void GameManager_OnClickedOnGridPosition(object sender, GameManager.OnClickedOnGridPositionEventArgs e)
+    {
+        Debug.Log("GMClick");
+        SpawnObjectRpc(e.x, e.y, e.playerType);
+    }
+
+    [Rpc(SendTo.Server)]
+    private void SpawnObjectRpc(int x , int y, GameManager.PlayerType playerType)
+    {
+        Debug.Log("SpawnObject");
+        Transform prefab;
+        switch (playerType)
+        {
+            default:
+            case GameManager.PlayerType.Cross:
+                prefab = crossPrefab;
+                break;
+            case GameManager.PlayerType.Circle:
+                prefab = circlePrefab;
+                break;
+        }
+        Transform spawnedCrossTransform = Instantiate(prefab, GetGridWorldPosition(x,y), Quaternion.identity);
+        spawnedCrossTransform.GetComponent<NetworkObject>().Spawn(true);
+        
+    }
+
+        private Vector2 GetGridWorldPosition(int x, int y)
+    {
+        return new Vector2(-GRID_SIZE + x* GRID_SIZE, -GRID_SIZE + y* GRID_SIZE);
+    }
+    
+}
